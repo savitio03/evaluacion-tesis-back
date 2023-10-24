213bcb61a61fef29735c72cc3d0c1cb5318923d9
savito03 <savioromero2019@gmail.com>
2023-10-24 12:51:08 -0500

    first commit

---
+++ src/main/java/com/unisinu/evaluaciontesis/EvaluaciontesisApplication.java
@@ -0,0 +1,13 @@
+package com.unisinu.evaluaciontesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluaciontesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluaciontesisApplication.class, args);
+	}
+
+}
